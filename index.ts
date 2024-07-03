d05d4901900cc80b2bb6ad74dab682bbd33fb9b0
Aston Haycock <astonhaycocks@gmail.com>
2024-07-03 17:01:14 -0600

    fixed files

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+// import model from "./model";
+// import cors from "cors";
+// import { OpenAI } from openai;
+// import multer from multer;
+
+const app = express();
+app.use(express.json({ limit: "10mb" }));
+app.use(express.static("public/recipe-vision-client"))
+
+
+
+app.listen(8080,() => {
+  console.log("running at http://localhost:8080")
+})

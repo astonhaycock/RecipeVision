a267500531c596dcee71eff18389154a73d8937c
Aston Haycock <astonhaycocks@gmail.com>
2024-07-04 09:30:51 -0600

    added nav bar

--- public/recipe-vision-client/src/router/index.ts
+++ public/recipe-vision-client/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/Login.vue')
     },
     {
       path: '/about',
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/ingredients',
+      name: 'ingredients',
+      component: () => import('../views/IngredientView.vue')
     }
   ]
 })

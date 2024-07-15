0787a84ffb3333d99fa593b2f8f25813d80453d4
Jarod Whiting <WhitingJarod@ProtonMail.com>
2024-07-15 09:55:38 -0600

    changed 'localhost' to '127...' due to an issue with Firefox and iMac.

--- dev/server/src/index.ts
+++ dev/server/src/index.ts
@@ -387,7 +387,7 @@ app.post(
 //================================================================================================//
 
 app.listen(PORT, () => {
-  console.log(`Server started: http://localhost:${PORT}/`);
+  console.log(`Server started: http://127.0.0.1:${PORT}/`);
 });
 
 // AAAAAAAAAAAAAAAAA

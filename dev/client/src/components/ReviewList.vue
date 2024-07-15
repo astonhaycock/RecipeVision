c56c1b5b44da426ce926835b1d006e61a06b8eac
Aston Haycock <astonhaycocks@gmail.com>
2024-07-15 12:39:10 -0600

    added delete button on review page

--- dev/client/src/components/ReviewList.vue
+++ dev/client/src/components/ReviewList.vue
@@ -1,82 +1,95 @@
 <script setup lang="ts">
-defineProps<{
-  reviewList: string;
-}>();
+  defineProps<{
+    reviewList: string;
+  }>();
 </script>
 <script lang="ts">
-import icon from "./icons/IconUpload.vue";
-import { defineComponent } from "vue";
-export default defineComponent({
-  data() {
-    return {};
-  },
-  methods: {},
-  computed: {},
-});
+  import icon from "./icons/IconUpload.vue";
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    data() {
+      return {
+        ingredients: [],
+        editing: false,
+      };
+    },
+    methods: {
+      deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
+      },
+      edit(item: string, index: number) {},
+    },
+    computed: {},
+  });
 </script>
 
 <template>
   <div id="list-container">
-    <h2>List of Ingredients</h2>
     <ul>
-      <li id="ingredient" v-for="ingredient in reviewList">
-        <p id="ingredient">{{ ingredient }}</p>
-        <button id="editBtn">Edit</button>
-        <button id="deleteBtn">Delete</button>
+      <li id="ingredient" v-for="(ingredient, index) in ingredients">
+        <p v-if="!editing" id="ingredient">{{ ingredient }}</p>
+        <input v-if="!editing" :placeholder="ingredient" />
+        <button v-if="!editing" id="editBtn" @click="editing = !editing">
+          Edit
+        </button>
+        <button v-if="!editing" id="editBtn" @click="editing = !editing">
+          Save
+        </button>
+        <button id="deleteBtn" @click="deleteIngredient(index)">Delete</button>
       </li>
     </ul>
   </div>
 </template>
 
 <style scoped>
-#list-container {
-  /* color: var(--vt-c-white-mute); */
-  height: 600px;
-  width: 400px;
-  /* overflow: hidden; */
-  overflow-y: scroll;
-  font-size: larger;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-}
-p {
-  text-transform: capitalize;
-}
+  #list-container {
+    /* color: var(--vt-c-white-mute); */
+    height: 600px;
+    width: 400px;
+    /* overflow: hidden; */
+    overflow-y: scroll;
+    font-size: larger;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+  p {
+    text-transform: capitalize;
+  }
 
-ul {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-}
-li {
-  display: flex;
-  justify-content: flex-start;
-  gap: 1rem;
-}
-#ingredient {
-  width: 25rem;
-  overflow-wrap: break-word;
-}
-li button {
-  border-radius: 15px 15px 15px 15px;
-  width: 70px;
-  border: none;
-  background-color: var(--vt-c-divider-dark-2);
-  color: var(--vt-c-text-dark-2);
-  transition: 400ms;
-}
-#editBtn:hover {
-  background-color: var(--vt-c-text-dark-2);
-}
-#deleteBtn {
-  background-color: var(--vt-c-delete-red);
-}
-#deleteBtn:hover {
-  background-color: var(--vt-c-delete-hover);
-}
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    display: flex;
+    justify-content: flex-start;
+    gap: 1rem;
+  }
+  #ingredient {
+    width: 25rem;
+    overflow-wrap: break-word;
+  }
+  li button {
+    border-radius: 15px 15px 15px 15px;
+    width: 70px;
+    border: none;
+    background-color: var(--vt-c-divider-dark-2);
+    color: var(--vt-c-text-dark-2);
+    transition: 400ms;
+  }
+  #editBtn:hover {
+    background-color: var(--vt-c-text-dark-2);
+  }
+  #deleteBtn {
+    background-color: var(--vt-c-delete-red);
+  }
+  #deleteBtn:hover {
+    background-color: var(--vt-c-delete-hover);
+  }
 </style>
--- dev/client/src/views/IngredientView.vue
+++ dev/client/src/views/IngredientView.vue
@@ -1,35 +1,36 @@
 <script setup lang="ts">
-import loading from "../assets/loading.gif";
-import IngredientList from "../components/IngredientList.vue";
-import ImageUpload from "../components/ImageUpload.vue";
-import ReviewList from "../components/ReviewList.vue";
-import { defineComponent } from "vue";
-import { ref, type Ref } from "vue";
+  import loading from "../assets/loading.gif";
+  import IngredientList from "../components/IngredientList.vue";
+  import ImageUpload from "../components/ImageUpload.vue";
+  import ReviewList from "../components/ReviewList.vue";
+  import { defineComponent } from "vue";
+  import { ref, type Ref } from "vue";
+  import DefaultButton from "../components/DefaultButton.vue";
 
-const ingredients: Ref<string[]> = ref([]);
-const ingredient = ref("#4400ff");
+  const ingredients: Ref<string[]> = ref([]);
+  const ingredient = ref("#4400ff");
 </script>
 
 <script lang="ts">
-export default defineComponent({
-  components: {
-    ImageUpload,
-  },
-  data() {
-    return {
-      modal: false,
-      loadingscreen: true,
-      reviewList: "",
-    };
-  },
-  methods: {
-    handleReviewIngredients: function (data: string) {
-      this.reviewList = data;
-      this.loadingscreen = false;
+  export default defineComponent({
+    components: {
+      ImageUpload,
     },
-  },
-  computed: {},
-});
+    data() {
+      return {
+        modal: true,
+        loadingscreen: false,
+        reviewList: "",
+      };
+    },
+    methods: {
+      handleReviewIngredients: function (data: string) {
+        this.reviewList = data;
+        this.loadingscreen = false;
+      },
+    },
+    computed: {},
+  });
 </script>
 
 <template>
@@ -44,13 +45,12 @@ export default defineComponent({
   <div v-if="modal" class="modal">
     <div class="modal-content">
       <div v-if="!loadingscreen">
+        <h1>Review List</h1>
         <ReviewList v-bind:reviewList="reviewList" />
-        <button class="updateBtn" @click="modal = false">
-          Save ingredients
-        </button>
-        <button class="updateBtn" @click="modal = false">
-          Save ingredients
-        </button>
+        <div id="reviewBtn">
+          <DefaultButton class="btn" @click="modal = false" msg="Discard" />
+          <DefaultButton class="btn" @click="modal = false" msg="Save List" />
+        </div>
       </div>
       <div id="loading-screen" v-else>
         <h3>Loading Ingredients</h3>
@@ -61,52 +61,63 @@ export default defineComponent({
 </template>
 
 <style scoped>
-#page {
-  padding-top: 1rem;
-  display: flex;
-  width: 80%;
-  justify-content: space-around;
-}
-#loading-screen {
-  display: flex;
-  flex-direction: column;
-  gap: 3rem;
-}
-#loading-screen img {
-  width: 250px;
-  height: 250px;
-}
-.modal {
-  height: 100vh;
-  background-color: rgba(24, 21, 21, 0.88);
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1;
-}
-.modal-content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 10rem;
-  gap: 1rem;
-  height: 100vh;
-  color: white;
-  text-align: center;
-  font-size: larger;
-}
-.modal-content input {
-  text-align: center;
-}
-.open {
-  filter: blur(18px);
-  overflow: hidden;
-}
+  #reviewBtn {
+    display: flex;
+    justify-content: center;
 
-@media (max-width: 1200px) {
-  #page {
-    flex-direction: column;
+    gap: 2rem;
   }
-}
+  .btn {
+    padding: 0.5rem;
+    text-align: center;
+    width: 10rem;
+  }
+  #page {
+    padding-top: 1rem;
+    display: flex;
+    width: 80%;
+    justify-content: space-around;
+  }
+  #loading-screen {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+  }
+  #loading-screen img {
+    width: 250px;
+    height: 250px;
+  }
+  .modal {
+    height: 100vh;
+    background-color: rgba(24, 21, 21, 0.88);
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+  }
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 5rem;
+    gap: 1rem;
+    height: 100vh;
+    color: white;
+    text-align: center;
+    font-size: larger;
+  }
+  .modal-content input {
+    text-align: center;
+  }
+  .open {
+    filter: blur(18px);
+    overflow: hidden;
+  }
+
+  @media (max-width: 1200px) {
+    #page {
+      flex-direction: column;
+    }
+  }
 </style>

74256d2410fa9b92122f39948b761996502f270e
Aston Haycock <astonhaycocks@gmail.com>
2024-07-15 11:10:40 -0600

    fixed bug

--- dev/client/src/App.vue
+++ dev/client/src/App.vue
@@ -1,54 +1,55 @@
 <script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-// import HelloWorld from "./components/HelloWorld.vue";
-import NavBar from "./components/NavBar.vue";
-import NavBarMobile from "./components/NavBarSmall.vue";
+  import { RouterLink, RouterView } from "vue-router";
+  // import HelloWorld from "./components/HelloWorld.vue";
+  import NavBar from "./components/NavBar.vue";
+  import NavBarMobile from "./components/NavBarSmall.vue";
 
-// `inject` is used for importing the global session data
-import { defineComponent, inject } from "vue";
+  // `inject` is used for importing the global session data
+  import { defineComponent, inject } from "vue";
 
-export default defineComponent({
-  components: {
-    NavBar,
-    NavBarMobile,
-  },
-  name: "App",
-  setup() {
-    // Import the global-level 'current-user' to track session info
-    const current_user: { email: string; IngredientList: string } = inject(
-      "current_user"
-    ) || { email: "error", IngredientList: "error" };
-    return { current_user };
-  },
-  data() {
-    return {
-      login: true,
-      message: "Hello, World!",
-      nav_open: false,
-    };
-  },
-  methods: {
-    handleNavOpen: function () {
-      this.nav_open = true;
+  export default defineComponent({
+    components: {
+      NavBar,
+      NavBarMobile,
     },
-    receiveUser(user: any) {
-      console.log(user);
+    name: "App",
+    setup() {
+      // Import the global-level 'current-user' to track session info
+      const current_user: { email: string; IngredientList: string } = inject(
+        "current_user"
+      ) || { email: "error", IngredientList: "error" };
+      return { current_user };
     },
+    data() {
+      return {
+        login: true,
+        message: "Hello, World!",
+        nav_open: false,
+      };
+    },
+    methods: {
+      handleNavOpen: function () {
+        this.nav_open = true;
+      },
+      receiveUser(user: any) {
+        console.log(user);
+      },
 
-    async getSession() {
-      const response = await fetch("https://dont-pani.ccani.cc/api/session");
-      if (response.status === 200 && (await response.json()) !== "") {
-        const data = await response.json();
-        this.current_user.email = data.email;
-        this.current_user.IngredientList = data.ingredient;
-      } else {
-        this.current_user.email = "error";
-        this.current_user.IngredientList = "error";
-      }
-      console.log(this.current_user);
-    },
-    created() {
-      this.getSession();
+      async getSession() {
+        const response = await fetch("https://dont-pani.ccani.cc/api/session");
+        if (response.status === 200 && (await response.json()) !== "") {
+          const data = await response.json();
+          this.current_user.email = data.email;
+          this.current_user.IngredientList = data.ingredient;
+        } else {
+          this.current_user.email = "error";
+          this.current_user.IngredientList = "error";
+        }
+        console.log(this.current_user);
+      },
+      created() {
+        this.getSession();
+      },
     },
   });
 </script>
@@ -60,19 +61,19 @@ export default defineComponent({
 </template>
 
 <style scoped>
-#mobileNav {
-  display: none;
-}
-#bigNav {
-  display: block;
-}
-
-@media (max-width: 900px) {
   #mobileNav {
-    display: block;
-  }
-  #bigNav {
     display: none;
   }
-}
+  #bigNav {
+    display: block;
+  }
+
+  @media (max-width: 900px) {
+    #mobileNav {
+      display: block;
+    }
+    #bigNav {
+      display: none;
+    }
+  }
 </style>

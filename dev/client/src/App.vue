c215f84a2c9b522d2c9ff046dfd858d98b89e3d3
Aston Haycock <astonhaycocks@gmail.com>
2024-07-16 15:23:05 -0600

    changed app.vue API

--- dev/client/src/App.vue
+++ dev/client/src/App.vue
@@ -1,57 +1,34 @@
-<script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-// import HelloWorld from "./components/HelloWorld.vue";
-import NavBar from "./components/NavBar.vue";
-import NavBarMobile from "./components/NavBarSmall.vue";
+<script setup lang="ts">
+  import { RouterLink, RouterView } from "vue-router";
+  // import HelloWorld from "./components/HelloWorld.vue";
+  import NavBar from "./components/NavBar.vue";
+  import NavBarMobile from "./components/NavBarSmall.vue";
 
-// `inject` is used for importing the global session data
-import { defineComponent, inject } from "vue";
+  // `inject` is used for importing the global session data
+  import { inject, ref } from "vue";
+  const login = ref(true);
+  const nav_open = ref(false);
+  const current_user: { email: string; IngredientList: string } = inject(
+    "current_user"
+  ) || { email: "error", IngredientList: "error" };
 
-export default defineComponent({
-  components: {
-    NavBar,
-    NavBarMobile,
-  },
-  name: "App",
-  setup() {
-    // Import the global-level 'current-user' to track session info
-    const current_user: { email: string; IngredientList: string } = inject(
-      "current_user"
-    ) || { email: "error", IngredientList: "error" };
-    return { current_user };
-  },
-  data() {
-    return {
-      login: true,
-      message: "Hello, World!",
-      nav_open: false,
-    };
-  },
-  methods: {
-    handleNavOpen: function () {
-      this.nav_open = true;
-    },
-    receiveUser(user: any) {
-      console.log(user);
-    },
-
-    async getSession() {
-      const response = await fetch("https://dont-pani.cc/api/session");
-      if (response.status === 200 && (await response.json()) !== "") {
-        const data = await response.json();
-        this.current_user.email = data.email;
-        this.current_user.IngredientList = data.ingredient;
-      } else {
-        this.current_user.email = "error";
-        this.current_user.IngredientList = "error";
-      }
-      console.log(this.current_user);
-    },
-    created() {
-      this.getSession();
-    },
-  },
-});
+  function handleNavOpen() {
+    nav_open.value = true;
+  }
+  function receiveUser(user: any) {
+    console.log(user);
+  }
+  async function getSession() {
+    const response = await fetch("https://dont-pani.cc/api/session");
+    if (response.status === 200 && (await response.json()) !== "") {
+      const data = await response.json();
+      current_user.email = data.email;
+      current_user.IngredientList = data.ingredient;
+    } else {
+      current_user.email = "error";
+      current_user.IngredientList = "error";
+    }
+  }
 </script>
 
 <template>
@@ -61,19 +38,19 @@ export default defineComponent({
 </template>
 
 <style scoped>
-#mobileNav {
-  display: none;
-}
-#bigNav {
-  display: block;
-}
-
-@media (max-width: 900px) {
   #mobileNav {
-    display: block;
-  }
-  #bigNav {
     display: none;
   }
-}
+  #bigNav {
+    display: block;
+  }
+
+  @media (max-width: 900px) {
+    #mobileNav {
+      display: block;
+    }
+    #bigNav {
+      display: none;
+    }
+  }
 </style>

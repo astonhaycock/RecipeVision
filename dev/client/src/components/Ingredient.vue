8150a4c11ed82319a052462351fb2ed2085db489
Aston Haycock <astonhaycocks@gmail.com>
2024-07-17 18:08:46 -0600

    test changes

--- dev/client/src/components/Ingredient.vue
+++ dev/client/src/components/Ingredient.vue
@@ -40,6 +40,7 @@
     if (response.status === 200) {
       items.value = data;
       console.log("Ingredient got Successfully");
+      console.log(data);
       console.log(items.value);
     } else {
       console.log("Ingredient not received");

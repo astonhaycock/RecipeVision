c0f278057a80d6bcc7181c031aa3fcb59e2024a9
Jarod Whiting <WhitingJarod@gmail.com>
2024-07-22 17:15:05 -0600

    more obvious drawer button

--- dev/client/src/components/Ingredient.vue
+++ dev/client/src/components/Ingredient.vue
@@ -1,96 +1,105 @@
 <script setup lang="ts">
-  import { ref, computed, watch, onMounted } from "vue";
-  import type { Ref } from "vue";
-  import ImageUpload from "./ImageUpload.vue";
-  import { useMediaQuery } from "@vueuse/core";
-  const mobile = useMediaQuery("(max-width: 800px)");
+import { ref, computed, watch, onMounted } from "vue";
+import type { Ref } from "vue";
+import ImageUpload from "./ImageUpload.vue";
+import { useMediaQuery } from "@vueuse/core";
+const mobile = useMediaQuery("(max-width: 800px)");
 
-  const items: Ref<Array<string>> = ref([]);
+const items: Ref<Array<string>> = ref([]);
 
-  const loading = ref(false);
-  const drawer = ref(false);
-  const search = ref("");
-  const selected: Ref<Array<string>> = ref([]);
-  const removedItems: Ref<Array<string>> = ref([]);
+const loading = ref(false);
+const drawer = ref(false);
+const search = ref("");
+const selected: Ref<Array<string>> = ref([]);
+const removedItems: Ref<Array<string>> = ref([]);
 
-  const allSelected = computed(() => selected.value.length === items.value.length);
+const allSelected = computed(
+  () => selected.value.length === items.value.length
+);
 
-  const categories = computed(() => {
-    const searchText = search.value.toLowerCase();
-    if (!searchText) return items.value;
+const categories = computed(() => {
+  const searchText = search.value.toLowerCase();
+  if (!searchText) return items.value;
 
-    return items.value.filter((item) => item.toLowerCase().includes(searchText));
-  });
+  return items.value.filter((item) => item.toLowerCase().includes(searchText));
+});
 
-  const toggleDrawerIcon = ref("mdi-chevron-right");
+// const toggleDrawerIcon = ref("mdi-chevron-right");
+const toggleDrawerIcon = ref("mdi-food-apple");
+const toggleDrawerIconColor = ref("red-darken-1");
 
-  watch(drawer, () => {
-    if (drawer.value) {
-      toggleDrawerIcon.value = "mdi-chevron-left";
-    } else {
-      toggleDrawerIcon.value = "mdi-chevron-right";
-    }
-  });
+watch(drawer, () => {
+  if (drawer.value) {
+    toggleDrawerIcon.value = "mdi-chevron-left";
+  } else {
+    toggleDrawerIcon.value = "mdi-food-apple";
+    // toggleDrawerIcon.value = "mdi-chevron-right";
+  }
+});
 
-  // const selections = selected.value;
+// const selections = selected.value;
 
-  function toggleDrawer() {
-    drawer.value = !drawer.value;
+function toggleDrawer() {
+  drawer.value = !drawer.value;
+  getIngredients();
+}
+
+watch(selected, () => {
+  search.value = "";
+});
+
+async function deleteList() {
+  console.log(selected.value);
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  loading.value = true;
+  const data = { items: selected.value };
+  const requestOptions = {
+    headers: myHeaders,
+    method: "DELETE",
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(
+    `${import.meta.env.VITE_PUBLIC_URL}/api/ingredients`,
+    requestOptions
+  );
+  if (response.status === 204) {
+    setTimeout(() => {
+      removedItems.value = removedItems.value.concat(selected.value);
+      items.value = items.value.filter(
+        (item) => !removedItems.value.includes(item)
+      );
+      selected.value = [];
+      loading.value = false;
+    }, 2000);
+    console.log("List deleted Successfully");
+  } else {
+    console.log("List not deleted");
+  }
+}
+
+async function getIngredients() {
+  removedItems.value = [];
+  console.log("Getting Ingredients...");
+  const response = await fetch(
+    `${import.meta.env.VITE_PUBLIC_URL}/api/ingredients`
+  );
+  const data = await response.json();
+  if (response.status === 200) {
+    items.value = data;
+    console.log("Ingredient got Successfully");
+  } else {
+    console.log("Ingredients not received");
+  }
+  function toggleIngredients() {
+    loading.value = !loading.value;
+  }
+}
+onMounted(() => {
+  if (!mobile) {
     getIngredients();
   }
-
-  watch(selected, () => {
-    search.value = "";
-  });
-
-  async function deleteList() {
-    console.log(selected.value);
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    loading.value = true;
-    const data = { items: selected.value };
-    const requestOptions = {
-      headers: myHeaders,
-      method: "DELETE",
-      body: JSON.stringify(data),
-    };
-    const response = await fetch(
-      `${import.meta.env.VITE_PUBLIC_URL}/api/ingredients`,
-      requestOptions
-    );
-    if (response.status === 204) {
-      setTimeout(() => {
-        removedItems.value = removedItems.value.concat(selected.value);
-        items.value = items.value.filter((item) => !removedItems.value.includes(item));
-        selected.value = [];
-        loading.value = false;
-      }, 2000);
-      console.log("List deleted Successfully");
-    } else {
-      console.log("List not deleted");
-    }
-  }
-
-  async function getIngredients() {
-    removedItems.value = [];
-    console.log("Getting Ingredients...");
-    const response = await fetch(`${import.meta.env.VITE_PUBLIC_URL}/api/ingredients`);
-    const data = await response.json();
-    if (response.status === 200) {
-      items.value = data;
-      console.log("Ingredient got Successfully");
-    } else {
-      console.log("Ingredients not received");
-    }
-    function toggleIngredients() {
-      loading.value = !loading.value;
-    }
-  }
-  onMounted(() => {
-    if (!mobile) {
-      getIngredients();
-    }
-  });
+});
 </script>
 
 <template>
@@ -103,7 +112,8 @@
       :mobile="mobile"
       :class="mobile ? 'elevation-0' : 'elevation-2 '"
       temporary
-      disable-route-watcher>
+      disable-route-watcher
+    >
       <!-- <v-fade-transition v-show="!drawer" mode="in-out" appear> -->
       <!-- </v-fade-transition> -->
       <v-container :class="mobile ? 'pr-10' : ''">
@@ -116,8 +126,13 @@
               v-for="(selection, i) in selected"
               :key="selection"
               class="py-1 pe-0"
-              cols="auto">
-              <v-chip :disabled="loading" closable @click:close="selected.splice(i, 1)">
+              cols="auto"
+            >
+              <v-chip
+                :disabled="loading"
+                closable
+                @click:close="selected.splice(i, 1)"
+              >
                 {{ selection }}
               </v-chip>
             </v-col>
@@ -129,7 +144,8 @@
                 :loading="loading"
                 color="purple"
                 variant="text"
-                @click="deleteList">
+                @click="deleteList"
+              >
                 Delete
               </v-btn>
             </v-card-actions>
@@ -139,7 +155,8 @@
                 v-model="search"
                 label="Search or Add"
                 hide-details
-                single-line></v-text-field>
+                single-line
+              ></v-text-field>
             </v-col>
           </v-row>
         </v-container>
@@ -153,7 +170,8 @@
               v-if="!selected.includes(item)"
               :key="item"
               :disabled="loading"
-              @click="selected.push(item)">
+              @click="selected.push(item)"
+            >
               <template v-slot:prepend>
                 <v-icon :disabled="loading"></v-icon>
               </template>
@@ -167,6 +185,8 @@
       </v-card>
       <v-btn
         @click="toggleDrawer"
+        id="toggle-drawer"
+        :color="toggleDrawerIconColor"
         :class="
           !mobile
             ? 'position-fixed top-0 right-0 mr-n16 mt-4'
@@ -174,23 +194,24 @@
             ? 'position-fixed top-0 right-0 mr-4 mt-4'
             : 'position-fixed top-0 right-0 mr-n16 mt-4'
         "
-        :icon="toggleDrawerIcon"></v-btn>
+        :icon="toggleDrawerIcon"
+      ></v-btn>
     </v-navigation-drawer>
   </v-container>
 </template>
 
 <style scoped>
-  #list-ingredients {
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-    align-items: center;
-    justify-content: flex-start;
-    height: 350px;
-    overflow: hidden;
-    overflow-y: scroll;
-  }
-  #ingredient-container {
-    height: 100vh;
-  }
+#list-ingredients {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  align-items: center;
+  justify-content: flex-start;
+  height: 350px;
+  overflow: hidden;
+  overflow-y: scroll;
+}
+#ingredient-container {
+  height: 100vh;
+}
 </style>

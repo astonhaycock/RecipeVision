33a3df6dca6d2f5d770eb1673e38c928e12cbc1f
Jarod Whiting <WhitingJarod@gmail.com>
2024-07-22 14:03:37 -0600

    added watcher service

--- dev/server/src/express/server.ts
+++ dev/server/src/express/server.ts
@@ -11,12 +11,14 @@ import { init as lists } from "./api/lists";
 import { init as openai } from "./api/openai";
 import { init as statics } from "./static";
 import { init as allrecipes } from "./api/allrecipes";
+import { init as generated_images } from "./generated_images";
 
-middleware(app);
-auth(app);
-lists(app);
-openai(app);
+await middleware(app);
+await auth(app);
+await lists(app);
+await openai(app);
 await statics(app);
-allrecipes(app);
+await allrecipes(app);
+await generated_images(app);
 
 export { app };

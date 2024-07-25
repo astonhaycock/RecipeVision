e234fca4ac62a3c368c8402eebc52886334a8bbf
Jarod Whiting <WhitingJarod@ProtonMail.com>
2024-07-24 21:10:27 -0600

    temporary mobile detection change

--- dev/client/src/main.ts
+++ dev/client/src/main.ts
@@ -21,7 +21,7 @@ const mobile_width = useMediaQuery("(max-width: 800px)");
 const mobile_aspect = useMediaQuery("(max-aspect-ratio: 5/8)");
 const mobile = ref(false);
 function mobile_update() {
-  mobile.value = mobile_width.value || mobile_aspect.value;
+  mobile.value = mobile_width.value && mobile_aspect.value;
 }
 watch([mobile_width, mobile_aspect], mobile_update);
 mobile_update();
